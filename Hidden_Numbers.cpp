5cc5234b0c40e5bc594cfd81f4d3271b3e9fd1f9
Yash <ys77357@gmail.com>
2023-01-05 10:09:14 +0530

    Day 69

---
+++ Hidden_Numbers.cpp
@@ -0,0 +1,21 @@
+#define ll long long
+#define f(end) for (int i = 0; i < end; i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        cout << 1 << " " << n << e;
+    }
+    return 0;
+}
+
+// we need to print 2 numbers ,every number has a 1 factor so we print 1 and the number itself
