ab42eb9f8fe4b8bb3296be8794edd9b817364144
Yash <ys77357@gmail.com>
2023-01-21 17:36:35 +0530

    Day 84

---
+++ Which_Division.cpp
@@ -0,0 +1,50 @@
+#define ll long long
+#define f(end) for (int i = 0; i < end; i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, r;
+    cin >> t;
+    while (t--)
+    {
+        cin >> r;
+        if (r < 1600)
+            cout << 3 << e;
+        else if (r >= 1600 && r < 2000)
+            cout << 2 << e;
+        else
+            cout << 1 << e;
+    }
+    return 0;
+}
+#define ll long long
+#define f(end) for (int i = 0; i < end; i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, r;
+    cin >> t;
+    while (t--)
+    {
+        cin >> r;
+        if (r < 1600)
+            cout << 3 << e;
+        else if (r >= 1600 && r < 2000)
+            cout << 2 << e;
+        else
+            cout << 1 << e;
+    }
+    return 0;
+}
+
+// we need to check the given condition and print aswer according to it
