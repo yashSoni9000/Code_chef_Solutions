d2de515832393c93b5bbaf5e76b0aa86d8c5f8b5
Yash <ys77357@gmail.com>
2023-06-23 14:45:52 +0530

    Day 234

---
+++ Valentine_Is_Coming.cpp
@@ -0,0 +1,20 @@
+#define ll long long
+#define f(end) for(int i=0;i<end;i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    cout<<x/y<<e;
+	}
+	return 0;
+}
+
+// chef has x rupees and each chocolate cost y we simply need to divide x/y to get the number of chocolates
