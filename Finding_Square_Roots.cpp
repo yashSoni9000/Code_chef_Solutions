499fd0fef041ff0ebc27b221ed688a78219f3374
Yash <ys77357@gmail.com>
2023-06-26 01:16:46 +0530

    Day 237

---
+++ Finding_Square_Roots.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+   int t;
+   cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << int(sqrt(n)) << endl;
+    }
+
+   return 0;
+}
+
+
+// we only need to imply the prebuilt function to solve this question
