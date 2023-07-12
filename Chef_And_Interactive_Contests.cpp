18dbde57c5d2636b92d13eec2ee494efae466666
Yash <ys77357@gmail.com>
2023-07-13 01:45:21 +0530

    Day 254

---
+++ Chef_And_Interactive_Contests.cpp
@@ -0,0 +1,18 @@
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t=1,n,r,temp;
+// 	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>r;
+	    while(n--){
+	        cin>>temp;
+	        if(temp>=r) cout<<"Good boi"<<e;
+	        else cout<<"Bad boi"<<e;
+	    }
+	}
+	return 0;
+}
+
+// we take the input and check if the given number is greater than r we print good boi else print bad boi
