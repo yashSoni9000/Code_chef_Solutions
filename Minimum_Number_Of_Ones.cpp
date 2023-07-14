23ef3b653782bb9ac4863269a46a5a774f32ad72
Yash <ys77357@gmail.com>
2023-07-14 23:58:17 +0530

    Day 255

---
+++ Minimum_Number_Of_Ones.cpp
@@ -0,0 +1,20 @@
+#define ll long long
+#define f(end) for(int i=0;i<end;i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    cout<<n/2<<e;
+	}
+	return 0;
+}
+
+// we simple need to print n/2 as irrespective of n answer is always n/2
