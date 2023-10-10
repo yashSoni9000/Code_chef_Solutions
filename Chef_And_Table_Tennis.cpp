22e2e086cfd7bdba7725ce92e15c6cf96b12a737
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2023-10-10 22:03:45 +0530

    Day 343

---
+++ Chef_And_Table_Tennis.cpp
@@ -0,0 +1,24 @@
+#define ll long long
+#define f(end) for(int i=0;i<end;i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int chef=0,other=0;
+	    bool ans;
+	    string s;
+	    cin>>s;
+	    if(s[s.length()-1]=='1') cout<<"WIN"<<e;
+	    else cout<<"LOSE"<<e;
+	}
+	return 0;
+}
+
+//whosoever wins the last round wins the game
