7f02558ea7d3a403ee74093fc55f89abc9cd4ec0
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-01-03 09:26:22 +0530

    Dat 427

---
+++ Count_The_Notebooks.cpp
@@ -0,0 +1,21 @@
+#define ll long long
+#define f(end) for(int i=0;i<end;i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    cout<<n*10<<e;
+	}
+	return 0;
+}
+
+// since the number of notebooks are per 100 pages and we receive 1000 per kg of pulp we divide 1000/100 =10 so we need to 
+// multiply with n kg of pulp to get number of notebook
