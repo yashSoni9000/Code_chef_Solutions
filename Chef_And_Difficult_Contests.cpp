668bca12f41c88deb620d587226d2cab4dedba3b
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-02-03 21:53:35 +0530

    Day 458

---
+++ Chef_And_Difficult_Contests.cpp
@@ -0,0 +1,20 @@
+#define ll long long
+#define f(end) for(int i=0;i<end;i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t,a,b;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b;
+	    cout<<"YES"<<e;
+	}
+	return 0;
+}
+
+// no matter what number is we can take d=1 and make both a and b equal with the given operations
