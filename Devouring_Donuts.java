b117bf31e785926461f66db9d7734ba20e38d793
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-05-11 23:53:09 +0530

    Day 556

---
+++ Devouring_Donuts.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t=1;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        int x,y;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        sout(x*y);
+    }
+}
+
+// we calculate total calories by multiplying both number 
