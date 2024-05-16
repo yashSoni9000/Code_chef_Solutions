0160048a91cb21cd339c264a2ab01a6178396954
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-05-16 23:54:57 +0530

    Day 561

---
+++ Fever.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        int x;
+        x = sc.nextInt();
+        if (x > 98) sout("YES");
+        else sout("NO");
+    }
+}
+
+// if x greater than 98 we print answer yes else answer is no
