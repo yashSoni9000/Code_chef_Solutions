cb47f8cef0762c79494723d108ef7ced92059b39
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-05-26 23:35:10 +0530

    Dag 571

---
+++ Chocolate_Distribution.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, n;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        n = sc.nextInt();
+        sout(((n + 1) / 2) + " " + n);
+    }
+}
+
+// given n there can be min number of children by calculatingupper limit of half of given chocolates and
+// n number to get the max number of children
