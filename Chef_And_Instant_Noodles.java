5f1045844bf2ad0a4ce2c407f3f2e29e5e70152f
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-06-15 21:04:48 +0530

    Day 590

---
+++ Chef_And_Instant_Noodles.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, x, y;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        y = sc.nextInt();
+        sout(x * y);
+    }
+}
+
+// we need to multiply given stoves with number of minutes to get total number of noodles
