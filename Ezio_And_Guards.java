377321f82eea249204cebfb5c3aa01a336a6470f
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-06-23 18:43:54 +0530

    Day 598

---
+++ Ezio_And_Guards.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x, y;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        y = sc.nextInt();
+        if (x >= y) sout("YES");
+        else sout("NO");
+    }
+}
+
+// if given x is greater than y then it is possible for ezio else not possible
