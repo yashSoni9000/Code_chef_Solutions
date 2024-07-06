9e116bd06785f8004c3a25a3351e80ea2c21dcca
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-07-06 20:39:14 +0530

    Day 611

---
+++ Height_Of_Rationals.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, a, b;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sout(Math.max(a, b));
+    }
+}
+
+// we need to print max number among given 2 the numerator and denominator part is given for confusion
