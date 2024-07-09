ed13005360cdc83c5aef9daf83da8592491e5247
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-07-09 20:48:17 +0530

    Day 614

---
+++ Minimum_Coins.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        sout(x % 10);
+    }
+}
+
+// the remaining of coins by paying from 10 rupee note will the number of coins, therefore i printed it
