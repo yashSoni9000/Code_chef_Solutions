7e1290284229b2c086e9f93b253ebe6f070bc76d
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-07-27 19:53:47 +0530

    Day 632

---
+++ Water_Requirement.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, n;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        n = sc.nextInt();
+        sout(n * 2);
+    }
+}
+
+// we need to print double for the number of liters to cool for n hours
