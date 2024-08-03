f084fe63e706b3ed7c19a7372a83d521312c570b
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-08-03 21:13:33 +0530

    Day 639

---
+++ Chess_Time.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, n;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        n = sc.nextInt();
+        sout(n * 3);
+    }
+}
+
+// we need to print given hours with 3 to get the number of matches and we print it
