8ff905bdb01cecd433d25d2e1fe0a19a98841e4f
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-08-21 20:05:32 +0530

    Day 657

---
+++ Just_One_More_Episode.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        sout(24 < x ? "YES" : "NO");
+    }
+}
+
+// if duration to exam is less than 24 print print no else print yes
