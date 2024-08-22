a44dab772d3556929a50502b279c7fcce6d5d5e7
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-08-22 21:40:39 +0530

    Day 658

---
+++ Get_Subscription.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        sout(x > 30 ? "YES" : "NO");
+    }
+}
+
+// if x > 30 print yes as he would need subscription else print no
