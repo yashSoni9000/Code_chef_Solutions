f8470deef74c1f77fd95a70a1582642ead2598d0
Yash Soni <84118997+yashSoni9000@users.noreply.github.com>
2024-08-26 21:05:13 +0530

    Day 662

---
+++ Chef_And_Donation.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x, y;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        y = sc.nextInt();
+        sout(y - x);
+    }
+}
+
+// since chefina always have more money than chef we need to print difference between chefina and chef to get our answer
