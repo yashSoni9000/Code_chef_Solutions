4b89e3e47dddb5be1107806ce7daf1a6622bf5a7
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-01 22:29:19 +0530

    Day 668

---
+++ Ageing.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        sout(x - 10);
+    }
+}
+
+// we need to print chef current age - 10 to get chefina age
