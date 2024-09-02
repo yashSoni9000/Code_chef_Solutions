b9ba5177d279be92cc7e66c7a5d7d882432ac2d5
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-02 19:23:10 +0530

    Day 669

---
+++ Battery_Health.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        if(x  >= 80) sout("YES");
+        else sout("NO");
+    }
+}
+
+// if x >= 80 print yes else print no
