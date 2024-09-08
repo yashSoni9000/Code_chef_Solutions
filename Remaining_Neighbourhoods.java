dda27a3e5fdc13295f5569426dd1f62813d78141
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-08 22:31:01 +0530

    Day 674

---
+++ Remaining_Neighbourhoods.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, n;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        n = sc.nextInt();
+        sout(100 - n);
+    }
+}
+
+// since he already visited n then remaining are 100 -n and we print it
