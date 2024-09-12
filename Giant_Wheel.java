60815fabb1da474464424d2aed075f932699b77b
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-12 20:00:45 +0530

    Day 678

---
+++ Giant_Wheel.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, x;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        sout(x >= 60 ? "YES" : "NO");
+    }
+}
+
+// if x >= 60 print yes else print no
