a5cc9358cf0e788cdc5b74e28b699719ce98a2cc
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-14 21:07:25 +0530

    Day 680

---
+++ Chef_On_Date.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x, y;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        y = sc.nextInt();
+        sout(x >= y ? "YES" : "NO");
+    }
+}
+
+// if x >= y print yes else print no
