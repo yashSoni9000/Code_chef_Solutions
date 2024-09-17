24cd94944232af150af91929cb5c2c4113b32003
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-18 01:18:37 +0530

    Day 683

---
+++ Best_Of_Two.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x, y;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        y = sc.nextInt();
+        sout(Math.max(x, y));
+    }
+}
+
+// we need to print max of given 2 numbers
