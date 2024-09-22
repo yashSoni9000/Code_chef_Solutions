b94fd3ea06637e60d2aa1aa8bd5062b44105dba5
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-22 20:32:43 +0530

    Day 686

---
+++ Reach_The_Target.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t, x, y;
+    t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        y = sc.nextInt();
+        sout(x - y);
+    }
+}
+
+// we need to print the difference between given 2 numbers
