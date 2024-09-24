0f2e8d79da8af1fc20d2288de9bbd68b2eefed54
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-24 20:59:03 +0530

    Day 688

---
+++ Chef_And_Brains_Speed.java
@@ -0,0 +1,16 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		// write your code here
+		if (y > x) System.out.println("YES");
+		else System.out.println("NO");
+	}
+}
+
+
+// if y > x print yes else print no
