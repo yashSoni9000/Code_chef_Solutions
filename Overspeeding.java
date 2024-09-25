46c683d4a325204ea9ac6785eb2e5095a5facf69
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-25 21:23:33 +0530

    Day 689

---
+++ Overspeeding.java
@@ -0,0 +1,11 @@
+FastReader sc = new FastReader();
+int t = 1, s;
+// t = sc.nextInt();
+while (t-- > 0) {
+    // Map < Integer, Integer > map = new HashMap < > ();
+    s = sc.nextInt();
+    if (s > 40) sout("YES");
+    else sout("NO");
+}
+
+// if given s is greater than 40 print yes else print no
