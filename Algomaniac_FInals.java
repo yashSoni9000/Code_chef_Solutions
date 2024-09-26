e4537f0d49f6ab457f050032ed58e7f2b3fd920f
Yash Soni <info.yashsoni9000@gmail.com>
2024-09-26 22:28:14 +0530

    Day 690

---
+++ Algomaniac_FInals.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, x;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        if (x == 17) sout("YAY");
+        else sout("NO");
+    }
+}
+
+// if he goes on 17 print yay else days print no
