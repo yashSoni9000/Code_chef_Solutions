9f16b365fb81a513d8221dd47f150fe5964a524f
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-01 21:31:26 +0530

    Day 695

---
+++ CodeChef_Learn_Problem_Solving.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, n;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        n = sc.nextInt();
+        sout(n * 2);
+    }
+}
+
+// for each course we have 2 in it so if n course then n*2 courses 
