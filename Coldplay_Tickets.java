152d6ca8866879aaed71c6962b3b0c969b21306c
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-03 20:35:44 +0530

    Day 697

---
+++ Coldplay_Tickets.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, n;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        n = sc.nextInt();
+        n++;
+        sout(n * 5000);
+    }
+}
+
+// chef has n friends so we add out ticket as well and multiply by 5000 to get out answer
