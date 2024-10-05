800e1eefb88268a1dfaa71347381bef0cb75ab77
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-05 20:02:25 +0530

    Day 699

---
+++ Sport_Section.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, x;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        if (x > 7) sout("YES");
+        else sout("NO");
+    }
+}
+
+// for sport section last 3 pages i.e. 8 9 10 so we can say that if given number is greater than 7 we print yes else print no
