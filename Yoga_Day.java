8e859b2c44e6195c776f53732a8b010f8caa3bce
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-06 20:58:05 +0530

    Day 700

---
+++ Yoga_Day.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, n;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        n = sc.nextInt();
+        sout(n / 12);
+    }
+}
+
+// since there are 12 posed in each round so if we divide given number of poses done by 12 we would get number of rounds
+// and print it
