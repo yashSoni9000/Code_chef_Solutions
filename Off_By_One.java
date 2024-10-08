c4bba4c73dbd93831bc7e5bc5b3f69704f52ae46
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-08 17:48:54 +0530

    Day 702

---
+++ Off_By_One.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, a, b;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sout(a + b + "" + 1);
+    }
+}
+
+// we add the given 2 numbers and add 1 as string and in order to do that you can simply put "" in between which will convert
+// anyting ahead to string
