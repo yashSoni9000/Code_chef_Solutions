0e20613d92246da5b71fee364e2c956e18a58ded
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-10 10:13:29 +0530

    Day 704

---
+++ Fitness.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            // Your code goes here
+            System.out.println(2 * x * 5);
+        }
+    }
+}
+
+// since chef goes to office and comes home so give x will be doubled to get the total distance and then we muliply by 5 days
+// to get total number of km chef has travelled
