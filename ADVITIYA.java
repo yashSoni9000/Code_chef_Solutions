c9befb691b17d370013eb91c122920849bd70ba7
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-23 21:05:58 +0530

    Day 716

---
+++ ADVITIYA.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, n;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        n = sc.nextInt();
+        if (n >= 16) sout("ADVITIYA");
+        else sout("WAITING FOR ADVITIYA");
+    }
+}
+
+// since fest will be on 16, 17, 18 so any number greater than or equal to 16 will be day he will be able to attend else
+// he would not be able to attend it
