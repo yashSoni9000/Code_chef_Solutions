7f2ee5ff7ce7401d07595468b3e924168f42c9a3
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-24 21:18:27 +0530

    Day 717

---
+++ Double_Rent.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, x;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        sout(x * 2);
+    }
+}
+
+// since owner doubled inital rent we need to multiply by 2 to get our answer
