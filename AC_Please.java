0425b4b5f4b8eacec0366bdd7252f2fa532b014d
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-25 21:16:20 +0530

    Day 718

---
+++ AC_Please.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, x;
+    // t = sc.nextInt();
+    while (t--> 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        if (x > 30) sout("YES");
+        else sout("NO");
+    }
+}
+
+// if given temperature is more than 30 print yes else print no
