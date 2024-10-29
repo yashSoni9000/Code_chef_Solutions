0f91d9c74a97a1fff94abf5f5ff3706cc9c0e680
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-29 20:40:01 +0530

    Day 722

---
+++ Summer_Time.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, x;
+    // t = sc.nextInt();
+    while (t--> 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        if (x > 35) sout("YES");
+        else sout("NO");
+    }
+}
+
+// we are given that if given temperature is more than 35 print yes else print no
