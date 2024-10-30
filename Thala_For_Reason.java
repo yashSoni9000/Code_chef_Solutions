09e7edda606cdb5db5db285579de82acabb2d192
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-30 17:23:23 +0530

    Day 723

---
+++ Thala_For_Reason.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, n;
+    // t = sc.nextInt();
+    while (t--> 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        n = sc.nextInt();
+        if (n == 7) sout("THALA");
+        else sout("SADGE");
+    }
+}
+
+// if given number is 7 print THALA else print SADGE
