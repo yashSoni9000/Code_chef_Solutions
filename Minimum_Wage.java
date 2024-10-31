23c046e32fe30509e8d5a6ced2a02699644b07ea
Yash Soni <info.yashsoni9000@gmail.com>
2024-10-31 20:02:51 +0530

    Day 724

---
+++ Minimum_Wage.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, x;
+    // t = sc.nextInt();
+    while (t--> 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        if (x > 11) sout("YES");
+        else sout("NO");
+    }
+}
+
+// if given wage x is greater than 11 pritn yes else print no
