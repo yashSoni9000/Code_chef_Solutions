21ab10396c4cfe0ca525d6e8ca53fd56896a02d4
Yash Soni <info.yashsoni9000@gmail.com>
2024-11-01 20:41:01 +0530

    Day 725

---
+++ Add_Two_Numbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--> 0)
+        {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            // write your code here
+            System.out.println(a + b);
+        }
+
+    }
+}
+
+// given in the question we need to add the given two numbers and print it
