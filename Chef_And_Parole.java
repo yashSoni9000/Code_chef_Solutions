2b749dfe9612d05ea9b0d9e84ce656e2fd61af76
Yash Soni <info.yashsoni9000@gmail.com>
2024-11-02 20:29:51 +0530

    Day 726

---
+++ Chef_And_Parole.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1, x;
+    // t = sc.nextInt();
+    while (t--> 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        x = sc.nextInt();
+        if (x >= 7) sout("YES");
+        else sout("NO");
+    }
+}
+
+// if given number is more than 6 print yes else print no
