568d4564707023404ada2b2eefd5dc016cf2824a
Yash Soni <info.yashsoni9000@gmail.com>
2024-11-03 20:52:38 +0530

    Day 727

---
+++ Lucky_Seven.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws java.lang.Exception
+{
+    FastReader sc = new FastReader();
+    int t = 1;
+    // t = sc.nextInt();
+    while (t-- > 0) {
+        // Map < Integer, Integer > map = new HashMap < > ();
+        String s = sc.next();
+        sout(s.charAt(6));
+    }
+}
+
+// we need to print char at 7th position and since indexing is 0 based therefore we find char at 6 to get 7 character
